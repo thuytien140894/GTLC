92f1b59cfdbf0db4b6264a5206e8c24e3f569bf8
Tien Ho <thuytien140894@gmail.com>
2018-03-26 13:05:27 -0700

    Added evaluation rules for casted terms

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -3,6 +3,7 @@ module Evaluator (
   ) where
 
     import Syntax
+    import Coercion
     
     import Data.Maybe
     import Data.Functor
@@ -14,17 +15,26 @@ module Evaluator (
       | isVal t1            = areAllVal ys
       | otherwise           = False
 
-    -- determine if a term is a value
-    isVal :: Term -> Bool
-    isVal t = case t of 
+    -- determine if a term is an uncoerced value
+    isUncoercedVal :: Term -> Bool
+    isUncoercedVal t = case t of 
       Unit                  -> True
       Tru                   -> True
       Fls                   -> True
       t' | isNumeric t'     -> True
       Lambda {}             -> True
-      Cast _ t | isVal t    -> True
       Rec ls                -> areAllVal ls
       _                     -> False
+
+    -- determine if a term is a coerced value
+    isCoercedValue :: Term -> Bool
+    isCoercedValue t = case t of 
+      Cast _ v | isUncoercedVal v  -> True 
+      _                            -> False
+
+    -- determine if a term is a value
+    isVal :: Term -> Bool
+    isVal t = isUncoercedVal t || isCoercedValue t
     
     -- determine if a term is a numeric value
     isNumeric :: Term -> Bool
@@ -86,31 +96,37 @@ module Evaluator (
     evaluate' :: Term -> Maybe Term
     evaluate' t = case t of
       -- Arithmetic
-      Pred Zero                           -> Just Zero                                -- (E-PREDZERO)
-      Pred (Succ nv) | isNumeric nv       -> Just nv                                  -- (E-PREDSUCC)
-      IsZero Zero                         -> Just Tru                                 -- (E-ISZEROZERO)
-      IsZero (Succ nv) | isNumeric nv     -> Just Fls                                 -- (E-ISZEROSUCC)
-      IsZero t1 | not (isNumeric t1)      -> IsZero <$> evaluate' t1                  -- (E-ISZERO)
-      Succ t1                             -> Succ <$> evaluate' t1                    -- (E-SUCC)
-      Pred t1                             -> Pred <$> evaluate' t1                    -- (E-PRED)
+      Pred Zero                           -> Just Zero                                    -- (E-PREDZERO)
+      Pred (Succ nv) | isNumeric nv       -> Just nv                                      -- (E-PREDSUCC)
+      IsZero Zero                         -> Just Tru                                     -- (E-ISZEROZERO)
+      IsZero (Succ nv) | isNumeric nv     -> Just Fls                                     -- (E-ISZEROSUCC)
+      IsZero t1 | not (isNumeric t1)      -> IsZero <$> evaluate' t1                      -- (E-ISZERO)
+      Succ t1                             -> Succ <$> evaluate' t1                        -- (E-SUCC)
+      Pred t1                             -> Pred <$> evaluate' t1                        -- (E-PRED)
 
       -- Conditional
-      If Tru t2 t3                        -> Just t2                                  -- (E-IFTRUE)
-      If Fls t2 t3                        -> Just t3                                  -- (E-IFFALSE)
-      If t1 t2 t3                         -> (\t1' -> If t1' t2 t3) <$> evaluate' t1  -- (E-IF)
+      If Tru t2 t3                        -> Just t2                                      -- (E-IFTRUE)
+      If Fls t2 t3                        -> Just t3                                      -- (E-IFFALSE)
+      If t1 t2 t3                         -> (\t1' -> If t1' t2 t3) <$> evaluate' t1      -- (E-IF)
+
+      -- Cast
+      Cast (Iden _) v | isUncoercedVal v  -> Just v                                       -- (E-CID)
+      App (Cast (Func c d) v1) v2 
+        | isUncoercedVal v1 && isVal v2   -> Cast d <$> evaluate' (App v1 (Cast c v2))    -- (E-CAPP)
+      Cast c (Cast d t)                   -> Cast (combineCoercions c d) <$> evaluate' t  -- (E-CCAST)
 
       -- Application
-      App (Lambda _ t1 _) v2 | isVal v2   -> Just $ subsFromTop v2 t1                 -- (E-APPABS)
-      App v1 t2 | isVal v1                -> App v1 <$> evaluate' t2                  -- (E-APP2)
-      App t1 t2                           -> (`App` t2) <$> evaluate' t1              -- (E-APP1)
+      App (Lambda _ t1 _) v2 | isVal v2   -> Just $ subsFromTop v2 t1                     -- (E-APPABS)
+      App v1 t2 | isVal v1                -> App v1 <$> evaluate' t2                      -- (E-APP2)
+      App t1 t2                           -> (`App` t2) <$> evaluate' t1                  -- (E-APP1)
 
       -- Records
-      Proj (Rec ls) l | isVal (Rec ls)    -> getVal (Rec ls) l                        -- (E-PROJRCD)
-      Proj (Rec ls) l                     -> (`Proj` l) <$> evaluate' (Rec ls)        -- (E-PROJ)
-      Rec ls | not (isVal (Rec ls))       -> evalRecord t                             -- (E-RCD)
-                      
+      Proj (Rec ls) l | isVal (Rec ls)    -> getVal (Rec ls) l                            -- (E-PROJRCD)
+      Proj (Rec ls) l                     -> (`Proj` l) <$> evaluate' (Rec ls)            -- (E-PROJ)
+      Rec ls | not (isVal (Rec ls))       -> evalRecord t                                 -- (E-RCD)
+                       
       -- No rules applied
-      _                                   -> Nothing                                  -- "Stuck"
+      _                                   -> Nothing                                      -- "Stuck"
 
     -- big-step evaluation
     -- (apply evaluate' repeatedly until a value is reached or we're left with an expression
