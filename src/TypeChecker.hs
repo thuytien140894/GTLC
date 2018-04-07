f8616c11115cd49bcfa01e6a0c2be4ac02e2e1aa
Tien Ho <thuytien140894@gmail.com>
2018-04-07 14:16:12 -0700

    Insert casts while typechecking instead of doing two tasks separately.

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,6 +1,7 @@
 module TypeChecker (
-  typeOf, 
-  isConsistent
+  typeOf,
+  typeCheck, 
+  insertCast
   ) where
 
   import Syntax
@@ -16,9 +17,21 @@ module TypeChecker (
     Right ty   -> (`addType` (l1, ty)) <$> rcdTypeOf (Rec ys) 
     Left err   -> Left err
 
-  -- add new entry to the record
+  -- find the type for a record
+  typeCheckRcd :: Term -> LabelIndex -> Either TypeError (Term, Type, LabelIndex)
+  typeCheckRcd (Rec []) l               = Right (Rec [], TRec [], l)
+  typeCheckRcd (Rec ((f1, t1) : ys)) l  = do 
+    (t1', ty, l1) <- typeCheck t1 l
+    (rcd, rcdTy, l2) <- typeCheckRcd (Rec ys) l1 
+    return (rcd `addField` (f1, t1'), rcdTy `addType` (f1, ty), l2)
+
+  -- add new entry to the record type
   addType :: Type -> (String, Type) -> Type
   addType (TRec ls) newType = TRec (newType : ls)
+
+  -- add new entry to the record
+  addField :: Term -> (String, Term) -> Term
+  addField (Rec ls) newField = Rec (newField : ls)
 
   -- get the type for the specified field
   getType :: Type -> String -> Either TypeError Type
@@ -26,6 +39,44 @@ module TypeChecker (
   getType (TRec ((l1, ty) : ys)) l 
     | l1 == l                    = Right ty
     | otherwise                  = getType (TRec ys) l
+
+  -- typecheck a record field
+  typeCheckField :: (Term, Type, LabelIndex) -> String -> Either TypeError (Term, Type, LabelIndex)
+  typeCheckField (_, TRec [], _) f                           = Left $ NotFound f 
+  typeCheckField (Rec ((_, t1) : xs), TRec ((f1, ty1) : ys), l) f 
+    | f1 == f                                                = Right (t1, ty1, l)
+    | otherwise                                              = typeCheckField (Rec xs, TRec ys, l) f
+
+  -- typecheck a conditional
+  typeCheckCond :: Term -> LabelIndex -> Either TypeError (Term, Type, LabelIndex)
+  typeCheckCond (If e1 e2 e3) l = do
+    (t1, cond, l1) <- typeCheck e1 l
+    (t2, fst, l2)  <- typeCheck e2 l1
+    (t3, snd, l3)  <- typeCheck e3 l2
+    case cond of 
+      Dyn 
+        | fst `isConsistent` snd -> Right (If (Cast c t1) t2 t3, fst, l3) 
+        | otherwise              -> Left $ Difference fst snd
+        where (c, l3) = coerce cond Bool l3
+      Bool 
+        | fst `isConsistent` snd -> Right (If t1 t2 t3, fst, l3) 
+        | otherwise              -> Left $ Difference fst snd
+      _                          -> Left $ NotBool cond
+
+  -- typecheck an application
+  typeCheckApp :: Term -> Term -> LabelIndex -> Either TypeError (Term, Type, LabelIndex)
+  typeCheckApp e1 e2 l = do                                            
+    (t1, funcTy, l1)  <- typeCheck e1 l  
+    (t2, argTy, l2)   <- typeCheck e2 l1
+    case funcTy of 
+      Dyn                              -> let (c, l3) = coerce argTy Dyn l2 in 
+                                          Right (App t1 $ Cast c t2, Dyn, l3)
+      Arr paramTy retTy 
+        | argTy `isCompatible` paramTy -> let (c, l3) = coerce argTy paramTy l2 in 
+                                          Right (App t1 $ Cast c t2, retTy, l3)
+        | otherwise                    -> Left $ Mismatch argTy paramTy
+        where (c, l3) = coerce argTy paramTy l2
+      _                                -> Left $ NotFunction t1
 
   -- two types are compatible if either they are subtypes of one another or 
   -- consistent
@@ -99,7 +150,64 @@ module TypeChecker (
                               Arr paramTy retTy 
                                 | argTy `isCompatible` paramTy -> Right retTy
                                 | otherwise                    -> Left $ Mismatch argTy paramTy
-                              _                                -> Left $ NotFunction t1
+                              _                                -> Left $ NotFunction t1                             
+
+  -- typecheck the source term and insert cast if needed
+  typeCheck :: Term -> LabelIndex -> Either TypeError (Term, Type, LabelIndex)
+  typeCheck e l = case e of 
+    Unit               -> Right (Unit, TUnit, l)                        -- (C-CONST)
+    Tru                -> Right (e, Bool, l)                                    
+    Fls                -> Right (e, Bool, l)                                   
+    Zero               -> Right (e, Nat, l)                                     
+
+    Succ e'            -> do                                            -- (C-SUCC)
+                            (t', ty, l1) <- typeCheck e' l
+                            case ty of  
+                              Dyn -> let (c, l2) = coerce ty Nat l1 in
+                                     Right (Succ $ Cast c t', Nat, l2)
+                              Nat -> Right (Succ t', Nat, l1)
+                              _   -> Left $ NotNat ty
+
+    Pred e'            -> do                                            -- (C-PRED)
+                            (t', ty, l1) <- typeCheck e' l
+                            case ty of
+                              Dyn -> let (c, l2) = coerce ty Nat l1 in
+                                     Right (Pred $ Cast c t', Nat, l2)
+                              Nat -> Right (Pred t', Nat, l1)
+                              _   -> Left $ NotNat ty
+
+    IsZero e'          -> do                                            -- (C-ISZERO)
+                            (t', ty, l1) <- typeCheck e' l 
+                            case ty of 
+                              Dyn  -> let (c, l2) = coerce ty Bool l1 in
+                                      Right (IsZero $ Cast c t', Bool, l2)
+                              Nat  -> Right (IsZero t', Bool, l1)
+                              _    -> Left $ NotNat ty
+
+    If e1 e2 e3        -> typeCheckCond e l                              -- (C-IF)
+                        
+    Rec ls             -> typeCheckRcd e l                               -- (C-RCD)
+
+    Proj e' f          -> case e' of                                     -- (C-PROJ)
+                            Rec ls -> do 
+                                        res <- typeCheck e' l 
+                                        typeCheckField res f 
+                            _      -> Left $ NotRecord e
+                        
+    Var _ ty _         -> case ty of                                     -- (C-VAR)
+                            TUnit -> Left $ NotBound e   
+                            _     -> Right (e, ty, l)                                  
+
+    Lambda ty e' ctx   -> do                                             -- (C-ABS)
+                            (t', retTy, l') <- typeCheck e' l           
+                            Right (Lambda ty t' ctx, Arr ty retTy, l')  
+
+    App e1 e2          -> typeCheckApp e1 e2 l                           -- (C-APP1 + C-APP2)
         
+  -- insert casts into a term
+  insertCast :: Term -> Either TypeError Term 
+  insertCast e = case typeCheck e 0 of 
+    Right (t, _, _)      -> Right t 
+    Left err             -> Left err
   
       
--- test/CastInsertionSpec.hs
+++ test/CastInsertionSpec.hs
@@ -1,8 +1,9 @@
 module CastInsertionSpec where
 
   import Test.Hspec
-  import CastInserter
+  import TypeChecker
   import Parser
+  import Errors
   import Types
   import Syntax
 
@@ -11,35 +12,35 @@ module CastInsertionSpec where
     describe "casts" $ do
       context "(\\x. 0) True" $
         it "should be (\\x. 0) <Bool!>True" $ 
-          insertCast (App (Lambda Dyn Zero ["x"]) Tru) 0 `shouldBe` 
-          Right (App (Lambda Dyn Zero ["x"]) (Cast (Inject Bool) Tru), 0)
+          insertCast (App (Lambda Dyn Zero ["x"]) Tru) `shouldBe` 
+          Right (App (Lambda Dyn Zero ["x"]) (Cast (Inject Bool) Tru))
 
       context "(\\x. (x 0)) (\\x:Nat. (succ x))" $
         it "should be (\\x. (x <Nat!>0)) <Nat?->Nat!>(\\x:Nat. (succ <Iden>x))" $ 
-          insertCast (App (Lambda Dyn (App (Var 0 Dyn "x") Zero) ["x"]) (Lambda Nat (Succ (Var 0 Nat "x")) ["x"])) 0 `shouldBe` 
-          Right (App (Lambda Dyn (App (Var 0 Dyn "x") (Cast (Inject Nat) Zero)) ["x"]) (Cast (Seq (Func (Project Nat 0) (Inject Nat)) (Inject (Arr Dyn Dyn))) (Lambda Nat (Succ (Cast (Iden Nat) (Var 0 Nat "x"))) ["x"])),1)
+          insertCast (App (Lambda Dyn (App (Var 0 Dyn "x") Zero) ["x"]) (Lambda Nat (Succ (Var 0 Nat "x")) ["x"])) `shouldBe` 
+          Right (App (Lambda Dyn (App (Var 0 Dyn "x") (Cast (Inject Nat) Zero)) ["x"]) (Cast (Seq (Func (Project Nat 0) (Inject Nat)) (Inject (Arr Dyn Dyn))) (Lambda Nat (Succ (Var 0 Nat "x")) ["x"])))
           
       context "(\\x:Dyn->Nat. (x 0)) (\\x:Nat. x)" $
         it "should be (\\x:Dyn->Nat. (x <Nat!>0)) <Nat?->Identity>(\\x:Nat. x)" $ 
-          insertCast (App (Lambda (Arr Dyn Nat) (App (Var 0 (Arr Dyn Nat) "x") Zero) ["x"]) (Lambda Nat (Var 0 Nat "x") ["x"])) 0 `shouldBe` 
-          Right (App (Lambda (Arr Dyn Nat) (App (Var 0 (Arr Dyn Nat) "x") (Cast (Inject Nat) Zero)) ["x"]) (Cast (Func (Project Nat 0) (Iden Nat)) (Lambda Nat (Var 0 Nat "x") ["x"])), 1)
+          insertCast (App (Lambda (Arr Dyn Nat) (App (Var 0 (Arr Dyn Nat) "x") Zero) ["x"]) (Lambda Nat (Var 0 Nat "x") ["x"])) `shouldBe` 
+          Right (App (Lambda (Arr Dyn Nat) (App (Var 0 (Arr Dyn Nat) "x") (Cast (Inject Nat) Zero)) ["x"]) (Cast (Func (Project Nat 0) (Iden Nat)) (Lambda Nat (Var 0 Nat "x") ["x"])))
 
       context "(\\x. succ x) true" $
         it "should be (\\x. succ <Nat?>x) <Bool!>true" $
-          insertCast (App (Lambda Dyn (Succ (Var 0 Dyn "x")) ["x"]) Tru) 0 `shouldBe` 
-          Right (App (Lambda Dyn (Succ (Cast (Project Nat 0) (Var 0 Dyn "x"))) ["x"]) (Cast (Inject Bool) Tru), 1)
+          insertCast (App (Lambda Dyn (Succ (Var 0 Dyn "x")) ["x"]) Tru) `shouldBe` 
+          Right (App (Lambda Dyn (Succ (Cast (Project Nat 0) (Var 0 Dyn "x"))) ["x"]) (Cast (Inject Bool) Tru))
 
       context "(\\x:Bool->Nat. succ 0) (\\x:Nat. x)" $
         it "should be (\\x:Bool->Nat. succ <Identity>0) <Fail>(\\x:Nat. x)" $
-          insertCast (App (Lambda (Arr Bool Nat) (Succ Zero) ["x"]) (Lambda Nat (Var 0 Nat "x") ["x"])) 0 `shouldBe` 
-          Right (App (Lambda (Arr Bool Nat) (Succ (Cast (Iden Nat) Zero)) ["x"]) (Cast (Fail (Arr Nat Nat) (Arr Bool Nat) 0) (Lambda Nat (Var 0 Nat "x") ["x"])),1)
+          insertCast (App (Lambda (Arr Bool Nat) (Succ Zero) ["x"]) (Lambda Nat (Var 0 Nat "x") ["x"])) `shouldBe` 
+          Left (Mismatch (Arr Nat Nat) (Arr Bool Nat))
 
       context "(\\x:Bool. x) 0" $
-        it "should be (\\x:Bool. x) <Fail>0" $
-          insertCast (App (Lambda Bool (Var 0 Bool "x") ["x"]) Zero) 0
-          `shouldBe` Right (App (Lambda Bool (Var 0 Bool "x") ["x"]) (Cast (Fail Nat Bool 0) Zero), 1)
+        it "should be Mismatch" $
+          insertCast (App (Lambda Bool (Var 0 Bool "x") ["x"]) Zero)
+          `shouldBe` Left (Mismatch Nat Bool)
 
       context "(\\x:Nat->Nat. x (x 0)) (\\x. (succ x))" $
         it "should be (\\x:Nat->Nat. x <Iden>(x <Iden>0)) <Nat!->Iden>(\\x. (succ x))" $
-          insertCast (App (Lambda (Arr Nat Nat) (App (Var 0 (Arr Nat Nat) "x") (App (Var 0 (Arr Nat Nat) "x") Zero)) ["x"]) (Lambda Dyn (Succ (Var 0 Dyn "x")) ["x"])) 0
-          `shouldBe` Right (App (Lambda (Arr Nat Nat) (App (Var 0 (Arr Nat Nat) "x") (Cast (Iden Nat) (App (Var 0 (Arr Nat Nat) "x") (Cast (Iden Nat) Zero)))) ["x"]) (Cast (Func (Inject Nat) (Iden Nat)) (Lambda Dyn (Succ (Cast (Project Nat 0) (Var 0 Dyn "x"))) ["x"])), 1)
+          insertCast (App (Lambda (Arr Nat Nat) (App (Var 0 (Arr Nat Nat) "x") (App (Var 0 (Arr Nat Nat) "x") Zero)) ["x"]) (Lambda Dyn (Succ (Var 0 Dyn "x")) ["x"]))
+          `shouldBe` Right (App (Lambda (Arr Nat Nat) (App (Var 0 (Arr Nat Nat) "x") (Cast (Iden Nat) (App (Var 0 (Arr Nat Nat) "x") (Cast (Iden Nat) Zero)))) ["x"]) (Cast (Func (Inject Nat) (Iden Nat)) (Lambda Dyn (Succ (Cast (Project Nat 0) (Var 0 Dyn "x"))) ["x"])))
