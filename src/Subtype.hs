3867baf3ce6fa739f143ea6f98dec5c88548937c
Tien Ho <thuytien140894@gmail.com>
2018-04-11 12:11:13 -0700

    Added subtyping rules for UD blame assignment.

--- src/Subtype.hs
+++ src/Subtype.hs
@@ -30,9 +30,19 @@ module Subtype (
     | l1 == l2                                = ty1 `isSubtype` ty2 && xs `isSubset` ys
     | otherwise                               = xs `isSubset` ((l2, ty2) : ys)
   
+  -- check if a type is a subtype of another
   isSubtype :: Type -> Type -> Bool
   isSubtype s Top                     = True                                      -- (S-TOP)
   isSubtype s t | s == t              = True                                      -- (S-REFL)
   isSubtype (Arr s1 s2) (Arr t1 t2)   = t1 `isSubtype` s1 && s2 `isSubtype` t2    -- (S-ARROW)
   isSubtype (TRec s) (TRec t)         = s `isRcdSubtype` t                        -- (S-RCD)
   isSubtype _ _                       = False 
+
+  -- subtyping rules for UD blame assignment
+  isSubtypeUD :: Type -> Type -> Bool
+  isSubtypeUD s t | s == t              = True
+  isSubtypeUD Bool Dyn                  = True                                      
+  isSubtypeUD Nat Dyn                   = True                                      
+  isSubtypeUD (Arr s1 s2) Dyn           = isSubtypeUD (Arr s1 s2) (Arr Dyn Dyn) 
+  isSubtypeUD (Arr s1 s2) (Arr t1 t2)   = t1 `isSubtypeUD` s1 && s2 `isSubtypeUD` t2
+  isSubtypeUD _ _                       = False     
