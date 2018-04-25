b2b6cf0a8b56afd4f5bb89e24663f2dd92e68774
Tien Ho <thuytien140894@gmail.com>
2018-04-25 15:33:50 -0700

    Added an option to run tests in the interpreter.

--- app/Main.hs
+++ app/Main.hs
@@ -18,9 +18,33 @@ module Main where
                             Left err -> printMsg err
       Left err         -> print err                       
 
+    test :: String -> IO ()
+    test s = do 
+      putStr "Test> "
+      putStrLn s
+      interpret s
+      putStrLn "\n" 
+
+    -- test cases 
+    runTests :: IO ()
+    runTests = do 
+      test "(\\m. ((\\x:Nat->Nat. (x 0)) m)) true" 
+      test "(\\x. succ x) true"
+      test "(\\m. ((\\x:Nat->Nat. (x 0)) m)) (\\y:Nat. iszero y)"
+      test "(\\x:Nat->Nat. x (x 0)) (\\x. (succ x))"
+      test "((\\m. if (\\x. iszero x) m then (\\x. succ x) else (\\x. pred x)) 0) true"
+      test "(\\x:Nat->Bool. x 0) (\\x:Nat. x)"
+      test "(\\n. (\\m. if (\\x. iszero x) n then m 0 else m (succ 0)) (\\y. succ y)) 0"
+      test "(\\x. (x 0)) (\\x:Nat. (succ x))"
+      test "(\\m. ((\\x:Nat->Bool. (x 0)) m)) (\\y:Nat. succ y)"
+      test "(\\m. ((\\x. (x 0)) m)) (\\y:Nat. succ y)"
+      test "(\\m. ((\\x. (x (succ (succ 0)))) m)) (\\y:Ref Nat. !y)"
+      test "(\\m. ((\\x. (x ref (succ (succ 0)))) m)) (\\y:Ref Nat. !y)"
+      test "(\\x. x:=succ 0) ref 0"
+
     -- print new line
-    printNewLine :: IO ()
-    printNewLine = putStrLn "\n"
+    newLine :: InputT IO ()
+    newLine = lift $ putStr "\n"
 
     -- run a read-eval-print loop
     loop :: InputT IO ()
@@ -28,10 +52,11 @@ module Main where
       input <- getInputLine "GTLC> "
       case input of
         Just "exit"  -> return ()
-        Just validIn -> lift (interpret validIn) >> lift printNewLine >> loop
+        Just "test"  -> lift runTests >> loop
+        Just validIn -> lift (interpret validIn) >> newLine >> loop
                             
     -- main method for the interpreter
     main :: IO ()
     main = do 
-      putStrLn "Gradually Typed Lambda Calculus, verion 1.0.0: https://github.com/thuytien140894/GTLC" 
+      putStrLn "Gradually Typed Lambda Calculus, verion 1.0.0: https://github.com/thuytien140894/GTLC"
       runInputT defaultSettings loop
